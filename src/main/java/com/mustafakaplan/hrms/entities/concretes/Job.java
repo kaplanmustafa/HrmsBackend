1233b6234d043eb6ea8e04f7ee9fbcb9e20e46fc
DESKTOP-0G02NO8\mustafakaplan <mstfakapln@gmail.com>
2021-05-22 22:48:45 +0300

    swagger integration

--- src/main/java/com/mustafakaplan/hrms/entities/concretes/Job.java
+++ src/main/java/com/mustafakaplan/hrms/entities/concretes/Job.java
@@ -21,5 +21,26 @@ public class Job {
     private double salary;
 
     @Temporal(TemporalType.TIMESTAMP)
-    private Date timestamp;
+    private Date startDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date endDate;
+
+    private int numberOfEmployees;
+
+    //company or employer
+
+    //city - country
+
+    //experience
+
+    //militaryStatus
+
+    //educationLevel
+
+    //department
+
+    //mannerOfWork
+
+    //social - benefits
 }
